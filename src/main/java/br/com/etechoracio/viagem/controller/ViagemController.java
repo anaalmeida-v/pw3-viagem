bf5e3790813705984d1642b2e9b8671c98b29458
Ana-Kaleb <AnaKaleb@example.com>
2023-09-12 11:05:05 -0300

    FindById, Post e Put

--- src/main/java/br/com/etechoracio/viagem/controller/ViagemController.java
+++ src/main/java/br/com/etechoracio/viagem/controller/ViagemController.java
@@ -4,11 +4,10 @@ package br.com.etechoracio.viagem.controller;
 import br.com.etechoracio.viagem.entity.Viagem;
 import br.com.etechoracio.viagem.repository.ViagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/viagens")
@@ -21,4 +20,23 @@ public class ViagemController {
     public List<Viagem> buscarTodos(){
         return repository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Optional<Viagem> buscarPorId(Long id){return repository.findById(id);}
+
+    @PostMapping
+    public Viagem inserir(@RequestBody Viagem body){
+        Viagem inserida = repository.save(body);
+        return inserida;
+    }
+
+    @PutMapping("/{id}")
+    public Viagem atualizar(@PathVariable Long id,
+                            @RequestBody Viagem obj){
+        Optional<Viagem> existe = buscarPorId(id);
+        if(existe.isPresent())
+            repository.save(obj);
+        return obj;
+    }
+
 }
